a0324c6e4f39e963c4c2f4672044b206666628f6
1samandarqarshiyev <140959149+1samandarqarshiyev@users.noreply.github.com>
2023-11-16 16:16:07 +0500

    ++

---
+++ src/blocks/functions/Item4.js
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+import './Main.css';
+
+function TagTypesExample() {
+  return (
+    <>
+      <Button className='modalbutton3' as="input" type="submit" value="Подтвердить"/>
+    </>
+  );
+}
+
+export default TagTypesExample;
--- src/blocks/functions/Main4.js
+++ src/blocks/functions/Main4.js
@@ -1,12 +1,78 @@
-import Button from 'react-bootstrap/Button';
-import './Main.css';
+// import Button from 'react-bootstrap/Button';
+// import './Main.css';
 
-function TagTypesExample() {
+// function TagTypesExample() {
+//   return (
+//     <>
+//       <Button className='block_end_button' as="input" type="submit" value="Oбратный звонок"/>
+//     </>
+//   );
+// }
+
+// export default TagTypesExample;
+
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import './Main.css';
+//
+import Item from './Item';
+import Item2 from './Item2';
+import Item3 from './Item3';
+import Item4 from './Item4';
+
+
+function Example() {
+  const [smShow, setSmShow] = useState(false);
+
+  function secondmain(){
+    setSmShow(true);
+  }
+
   return (
     <>
-      <Button className='block_end_button' as="input" type="submit" value="Oбратный звонок"/>
+      <Button onClick={secondmain} as="input" type="submit" className="me-2 block_end_button" value="Oбратный звонок"/>
+      
+      <Modal size="sm" show={smShow} onHide={() => setSmShow(false)} aria-labelledby="example-modal-sizes-title-sm">
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">
+            <p className='modaltext1'>Главная Горшки</p>
+            <p className='modaltext2'>Обратный звонок</p>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+
+            <Row>
+              <Col xl={6} lg={6} md={6} sm={6} xs={6} s={12}>
+                <Item/>
+              </Col>
+              <Col xl={6} lg={6} md={6} sm={6} xs={6} s={12}>
+                <Item2/>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xl={12} lg={12} md={12} sm={12} xs={12} s={12}>
+                <Item3/>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xl={12} lg={12} md={12} sm={12} xs={12} s={12}>
+                <Item4/>
+              </Col>
+            </Row>
+
+          </Container>
+        </Modal.Body>
+      </Modal>
+
     </>
   );
 }
 
-export default TagTypesExample;
+export default Example;
